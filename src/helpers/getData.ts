114cc9f298c038964c09d00d151cd73fb9312b67
GurgenBayramyan <gugobayramyan@gmail.com>
2023-07-01 16:17:26 +0400

    commit for fixet change interfaces

--- src/helpers/getData.ts
+++ src/helpers/getData.ts
@@ -2,7 +2,7 @@ import { Country } from "../ts";
 
 const basicUrl = "https://restcountries.com/v3.1/all"
 export const getData = async(): Promise<Country[]> =>{
-   let start = Math.floor(Math.random()*249)
+   let start:number = Math.floor(Math.random()*249)
    const response = await fetch(basicUrl);
    const data = await response.json();
    return await data.slice(start,start+2)
